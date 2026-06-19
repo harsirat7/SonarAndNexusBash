f52d0178157c08f9a3544aa610421ee51ca760ce
harsirat7 <harsirat757@gmail.com>
2026-06-19 22:58:27 +0530

    Adding Nexus

--- Nexus.sh
+++ Nexus.sh
@@ -5,3 +5,28 @@ apt-get install -y git wget unzip
 
 # creating user 
 sudo useradd -r -m -s /bin/bash nexus
+
+cd /opt
+
+#importing tar file
+wget https://download.sonatype.com/nexus/3/nexus-3.93.0-06-linux-x86_64.tar.gz
+tar -xvzf latest-linux-x86_64.tar.gz
+rm -rf nexus-3.93.0-06-linux-x86_64.tar.gz
+
+#renaming file
+sudo mv nexus-* nexus
+
+#changing ownership
+sudo chown -R nexus:nexus /opt/nexus
+
+#creating working directory
+sudo mkdir -p /opt/sonatype-work
+sudo chown -R nexus:nexus /opt/sonatype-work
+
+#importing servive file
+wget https://raw.githubusercontent.com/harsirat7/SonarAndNexusBash/refs/heads/master/nexus.service
+
+mv nexus.service /etc/systemd/system/nexus.service
+systemctl daemon-reload
+systemctl enable nexus
+systemctl start nexus
